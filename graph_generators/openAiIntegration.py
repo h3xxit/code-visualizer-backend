ae2a6a7a41a165deabca82b816e6b53e93d8a8f4
Peter Johanns <johannspeter@web.de>
2023-11-19 03:30:50 +0100

    created openapi endpoint to chat with a package

--- graph_generators/openAiIntegration.py
+++ graph_generators/openAiIntegration.py
@@ -4,7 +4,11 @@ import json
 function_description_promt = "You will recieve the code of a python file as input and a short description of each internal and external function which is called from the code. " + \
                              "Extract the functions and methods defined in the code and summarize each of them in 2 short sentences."
 
-
+ask_graph_prompt = "You are a chatbot that a user can interact with. the chat is part of an application that gives a graphical overview of a python code repository at different granularities. " + \
+    "The repository is presented as a graph of nodes where a node can either be a module, a file, a class or a function. You will recieve the message of the user and the currently displayed graph as json input. " + \
+    "Answer the message of theuser according to currently displayed graph while taking previously asked questions and graphs into account. If you refer to a node in your answer, use the name of the node and wrap it " + \
+    "in double curly brackets, e.g. {{node_name}}." 
+    
 def annotate_file(file_content: str, function_call_desc: str) -> dict[str, list]:
     file_content = f"Code: \n{file_content}\nRelated functions:\n{function_call_desc}"
     system_prompt = {
@@ -47,10 +51,28 @@ def annotate_file(file_content: str, function_call_desc: str) -> dict[str, list]
     response = openai.OpenAI().chat.completions.create(
         model="gpt-3.5-turbo",#"gpt-4-1106-preview",
         messages=[system_prompt, {"role": "user", "content": file_content}],
-        tools=tools
+        tools=tools, temperature=0.,
     )
     print("Finished openAI request")
 
     json_response = response.choices[0].message.tool_calls[0].function.arguments
     functions: dict[str, list] = json.loads(json_response)
     return functions
+
+
+def ai_quiry_with_graph(graph_str: str, messages : list[str]):
+    system_prompt = {
+        "role": "system",
+        "content": ask_graph_prompt,
+    }
+
+    print("Made a request to openAI")
+    response = openai.OpenAI().chat.completions.create(
+        model="gpt-3.5-turbo",#"gpt-4-1106-previewprint",
+        messages=[system_prompt, {"role": "user", "content": graph_str}, {"role": "user", "content": messages}],
+        temperature=0.,
+    )
+    print("Finished openAI request")
+
+    ai_response = response.choices[0].message.content
+    return json_response
